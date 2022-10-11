d8006ecce4ed78d44615b87e1522e610552d0131
Grigory-PC <g.trubnikov@mail.ru>
2022-10-11 12:04:26 +0300

    ref: cкорректировать переменные в application properties

--- main/src/main/java/ru/practicum/explorewithme/config/HibernateConfig.java
+++ main/src/main/java/ru/practicum/explorewithme/config/HibernateConfig.java
@@ -55,7 +55,7 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("spring.jpa.hibernate.dialect", environment.getRequiredProperty("spring.jpa.hibernate.dialect"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         return properties;
     }
